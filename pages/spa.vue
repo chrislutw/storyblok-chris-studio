5951bf9f1b3221c7ea06214ba39e13cedb844eec
chrislutw <kshke0723@gmail.com>
2023-11-14 20:08:32 +0800

    新增spa頁面

---
+++ pages/spa.vue
@@ -0,0 +1,9 @@
+<script setup>
+const story = await useAsyncStoryblok('home', {
+  version: useRoute().query._storyblok ? 'draft' : 'published'
+})
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
