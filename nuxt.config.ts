3280291eaae4f58be349633f4fcd0789f335cc50
chrislutw <kshke0723@gmail.com>
2023-11-14 19:03:07 +0800

    更新頁面render方式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,16 @@ export default defineNuxtConfig({
     '@storyblok/nuxt',
     '@nuxtjs/tailwindcss'
   ],
+  routeRules: {
+    // this page is generated at build time and cached permanently
+    '/': { prerender: true },
+    // this page will be revalidated every 60 seconds, in the background
+    '/cron': { isr: 60 },
+    // this page will be generated on demand and then cached permanently
+    '/static': { isr: true },
+    // this page will be always fresh
+    '/dynamic': { isr: false },
+  },
   storyblok: {
     accessToken: process.env.STORYBLOK_TOKEN,
     apiOptions: {
